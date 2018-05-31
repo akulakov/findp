cdd1d15ec1ca780490c3c567e7a868a2095d2cd0
AK <andrei.avk@gmail.com>
2018-05-31 09:24:50 -0400

    added nested mocking to test

--- findp_proj/findp/tests.py
+++ findp_proj/findp/tests.py
@@ -1,18 +1,20 @@
 from django.test import TestCase
 from django.core.management import call_command
-from unittest.mock import MagicMock, patch
+from unittest.mock import MagicMock, Mock, patch
 
 from findp.models import RecordP
 
 class TestCountP(TestCase):
-    @patch('findp.management.commands.count_p.Command.reddit_class')
-    def test_count_p(self, MockClass):
+    def test_count_p(self):
         "Test count_p command."
-        # import findp
-        # findp.management.commands.count_p.Command.praw_class = MagicMock
-        args = ['learnpython', 'how', 10]
-        call_command('count_p', *args)
-        rec = RecordP.objects.first()
-        self.assertEquals(rec.forum, 'learnpython')
-        self.assertEquals(rec.pattern, 'how')
-        self.assertEquals(rec.limit, 10)
+        with patch('findp.management.commands.count_p.Command.reddit_class') as reddit_mock:
+            subm_mock = MagicMock()
+            subm_mock.selftext = 'hello how how hi hi how'
+            reddit_mock.return_value.subreddit.return_value.hot.return_value = [subm_mock]
+            args = ['learnpython', 'how', 10]
+            call_command('count_p', *args)
+            rec = RecordP.objects.first()
+            self.assertEquals(rec.forum, 'learnpython')
+            self.assertEquals(rec.pattern, 'how')
+            self.assertEquals(rec.limit, 10)
+            self.assertEquals(rec.count, 3)
